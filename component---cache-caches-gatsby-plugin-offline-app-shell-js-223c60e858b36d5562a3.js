ef312029ba449979d402e1ef3cdd157f6c29d7c7
Ngoc Dao <ndao@atlassian.com>
2022-10-20 09:03:18 +1100

    Remove gatsby offline plugin

--- component---cache-caches-gatsby-plugin-offline-app-shell-js-223c60e858b36d5562a3.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfuture_development_constructions=self.webpackChunkfuture_development_constructions||[]).push([[306],{1367:function(e,t,n){var u=n(4836);t.__esModule=!0,t.default=void 0;var r=u(n(7867)),o=u(n(7294)),l=function(e){function t(){return e.apply(this,arguments)||this}return(0,r.default)(t,e),t.prototype.render=function(){return o.default.createElement(o.default.Fragment,null)},t}(o.default.Component),f=l;t.default=f}}]);
-//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-223c60e858b36d5562a3.js.map
